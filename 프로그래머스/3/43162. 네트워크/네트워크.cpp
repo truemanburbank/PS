1b4e5609a00d1278a82a2fbb95acab31f1eb38ca
truemanburbank <71928366+truemanburbank@users.noreply.github.com>
2025-06-13 16:15:48 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -3,67 +3,32 @@
 
 using namespace std;
 
-// union - find
-class DisjointSet
+vector<bool> visited;
+
+// dfs
+void dfs(const vector<vector<int>>& network, int node)
 {
-    vector<int> parent, rank;
+    visited[node] = true;
 
-public:
-    DisjointSet(int size) : parent(size, -1), rank(size, 0) {}
-
-    int Find(int node)
-    {
-        if (parent[node] == -1)
-            return node;
-
-        // 경로 압축
-        return parent[node] = Find(parent[node]);
-    }
-
-    void MakeUnion(int node1, int node2)
-    {
-        int root1 = Find(node1);
-        int root2 = Find(node2);
-
-        if (root1 != root2)
-        {
-            if (rank[root1] < rank[root2])
-                parent[root1] = root2;
-            else if (rank[root2] < rank[root1])
-                parent[root2] = root1;
-            else
-            {
-                parent[root1] = root2;
-                rank[root2]++;
-            }
-        }
-    }
-
-    bool IsCycle(int node1, int node2) { return Find(node1) == Find(node2); }
-
-    int GetRootCount()
-    {
-        int rootCount = 0;
-        for (int i = 0; i < parent.size(); i++)
-            if (parent[i] == -1)
-                rootCount++;
-
-        return rootCount;
-    }
-};
+    for (int i = 0; i < network[node].size(); i++)
+        if (network[node][i] == 1 && !visited[i])
+            dfs(network, i);
+}
 
 int solution(int n, vector<vector<int>> computers) 
 {
+    visited = vector<bool>(computers.size(), false);
+
     int answer = 0;
 
-    DisjointSet disjointSet(n);
-
     for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            if (computers[i][j] == 1 && j != i)
-                disjointSet.MakeUnion(i, j);
-
-    answer = disjointSet.GetRootCount();
+    {
+        if (!visited[i])
+        {
+            dfs(computers, i);
+            answer++;
+        }
+    }
 
     return answer;
 }
